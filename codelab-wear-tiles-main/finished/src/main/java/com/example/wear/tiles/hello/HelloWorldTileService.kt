3ef943c6a9296d33bfc52bd792a67a51b61ad3f1
Minh Pham <pdminh0410@gmail.com>
2025-02-06 16:57:05 -0800

    Initialize project

---
+++ codelab-wear-tiles-main/finished/src/main/java/com/example/wear/tiles/hello/HelloWorldTileService.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.wear.tiles.hello
+
+import androidx.wear.protolayout.DimensionBuilders
+import androidx.wear.protolayout.LayoutElementBuilders
+import androidx.wear.protolayout.ResourceBuilders
+import androidx.wear.protolayout.TimelineBuilders
+import androidx.wear.tiles.RequestBuilders
+import androidx.wear.tiles.TileBuilders
+import com.example.wear.tiles.R
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import com.google.android.horologist.tiles.SuspendingTileService
+
+private const val RESOURCES_VERSION = "0"
+
+@ExperimentalHorologistApi
+class HelloWorldTileService : SuspendingTileService() {
+
+    override suspend fun resourcesRequest(
+        requestParams: RequestBuilders.ResourcesRequest
+    ): ResourceBuilders.Resources {
+        return ResourceBuilders.Resources.Builder()
+            .setVersion(RESOURCES_VERSION)
+            .build()
+    }
+
+    override suspend fun tileRequest(
+        requestParams: RequestBuilders.TileRequest
+    ): TileBuilders.Tile {
+        val singleTileTimeline = TimelineBuilders.Timeline.Builder()
+            .addTimelineEntry(
+                TimelineBuilders.TimelineEntry.Builder()
+                    .setLayout(
+                        LayoutElementBuilders.Layout.Builder()
+                            .setRoot(tileLayout())
+                            .build()
+                    )
+                    .build()
+            )
+            .build()
+
+        return TileBuilders.Tile.Builder()
+            .setResourcesVersion(RESOURCES_VERSION)
+            .setTileTimeline(singleTileTimeline)
+            .build()
+    }
+
+    private fun tileLayout(): LayoutElementBuilders.LayoutElement {
+        val text = getString(R.string.hello_tile_body)
+        return LayoutElementBuilders.Box.Builder()
+            .setVerticalAlignment(LayoutElementBuilders.VERTICAL_ALIGN_CENTER)
+            .setWidth(DimensionBuilders.expand())
+            .setHeight(DimensionBuilders.expand())
+            .addContent(
+                LayoutElementBuilders.Text.Builder()
+                    .setText(text)
+                    .build()
+            )
+            .build()
+    }
+}
